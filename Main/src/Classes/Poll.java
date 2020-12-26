304bf76b359763e036afb8613ffa924d73b7a38c
Ethan Loev <ecloev@outlook.com>
2020-12-26 00:41:42 -0500

    Added IDs for Polls

--- Main/src/Classes/Poll.java
+++ Main/src/Classes/Poll.java
@@ -1,30 +1,28 @@
 package Classes;
 
+import java.util.UUID;
+
 public class Poll {
     private String name;
     private int maxResponses;
-    private long id;
+    private String id;
 
-    public Poll (String name, int maxResponses, long id) {
+    public Poll (String name, int maxResponses) {
         this.name = name;
         this.maxResponses = maxResponses;
-        this.id = id;
+        this.id = UUID.randomUUID().toString();
     }
 
     public int getMaxResponses() {
         return this.maxResponses;
     }
 
-    public long getId() {
+    public String getId() {
         return this.id;
     }
 
     public String getName() {
         return this.name;
-    }
-
-    public void setId(long id) {
-        this.id = id;
     }
 
     public void setMaxResponses(int maxResponses) {
