c29d75bf16185d6867f5dcc789fdef833522e06e
Ethan Loev <ecloev@outlook.com>
2020-12-27 15:21:28 -0500

    Added to Poll.java

--- Main/src/Classes/Poll.java
+++ Main/src/Classes/Poll.java
@@ -1,16 +1,21 @@
 package Classes;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class Poll {
     private String name;
     private int maxResponses;
     private final String id;
+    private ArrayList<String> questions;
+    private ArrayList<String[]> answerChoices;
 
     public Poll (String name, int maxResponses) {
         this.name = name;
         this.maxResponses = maxResponses;
         this.id = UUID.randomUUID().toString();
+        questions = new ArrayList<>();
+        answerChoices = new ArrayList<>();
     }
 
     public int getMaxResponses() {
