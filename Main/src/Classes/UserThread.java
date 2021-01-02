d2d768649a408883d1a9729fc49d1e231fc3e71c
Ellis Lacy <ellis.h.lacy@gmail.com>
2021-01-02 15:58:31 -0500

    update

--- Main/src/Classes/UserThread.java
+++ Main/src/Classes/UserThread.java
@@ -27,7 +27,6 @@ public class UserThread extends Thread {
     }
 
     public void run() {
-        String tempUsername = "";
         try {
             InputStream input = socket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
@@ -35,8 +34,25 @@ public class UserThread extends Thread {
             OutputStream output = socket.getOutputStream();
             writer = new PrintWriter(output, true);
 
+            String response = reader.readLine();
+            if (response.equals("1")) {
+                String pollID = reader.readLine();
+                String question = reader.readLine();
+                ArrayList<String> answers = new ArrayList<>();
+                while(true) {
+                    response = reader.readLine();
+                    if (response != null) {
+                        answers.add(reader.readLine());
+                    } else {
+                        break;
+                    }
+                }
+                createPoll(pollID, question, answers);
+            } else if ( response.equals("2")) {
+
+            }
             while (true) {
-                String response = reader.readLine();
+
 
                 if (response != null) {
 
@@ -45,6 +61,21 @@ public class UserThread extends Thread {
         } catch (IOException ex) {
             //server.removeUser(tempUsername, this);
             System.out.println("A user has Disconnected.");
+        }
+    }
+
+    public void createPoll(String pollID, String question, ArrayList<String> answers) {
+        File users = new File("Polls/" + pollID + ".txt");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(users, true))) {
+            bw.write(question);
+            bw.newLine();
+            for (int i = 0; i < answers.size(); i++) {
+                bw.write(answers.get(i));
+                bw.newLine();
+            }
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
