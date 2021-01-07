a358c7e49f6bf27126e4ed4d7c0640324e1dbadb
ehlacy01 <70302106+ehlacy01@users.noreply.github.com>
2021-01-06 19:16:16 -0500

    Update UserThread.java

--- Main/src/Classes/UserThread.java
+++ Main/src/Classes/UserThread.java
@@ -1,5 +1,7 @@
 package Classes;
 
+import Exceptions.PollNotFoundException;
+
 import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -27,7 +29,6 @@ public class UserThread extends Thread {
 
     public void run() {
         try {
-            System.out.println("TESTING DONE HERE");
             InputStream input = socket.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
@@ -35,26 +36,24 @@ public class UserThread extends Thread {
             writer = new PrintWriter(output, true);
 
             String response = reader.readLine();
-            System.out.println(response);
             if (response.equals("1")) {
                 String pollID = reader.readLine();
-                System.out.println("pollID: "+pollID);
                 String question = reader.readLine();
-                System.out.println("question: "+question);
                 ArrayList<String> answers = new ArrayList<>();
                 int numberOfAnswers = reader.read();
-                System.out.println(numberOfAnswers);
                 reader.readLine();
                 for (int i = 0; i < numberOfAnswers; i++) {
                     response = reader.readLine();
-                    System.out.println("response: "+response);
                     answers.add(response);
                 }
-
-                System.out.println("Testing: Create poll starts");
                 createPoll(pollID, question, answers);
             } else if (response.equals("2")) {
-
+                String pollID = reader.readLine();
+                try {
+                    flushPollData(pollID);
+                } catch (PollNotFoundException e) {
+                    e.printStackTrace();
+                }
             } else if (response.equals("3")) {
 
             }
@@ -66,11 +65,10 @@ public class UserThread extends Thread {
 
     public void createPoll(String pollID, String question, ArrayList<String> answers) {
         File users = new File("Polls/" + pollID + ".txt");
-        if (users.exists()) {
-
-        }
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(users, true))) {
             bw.write(question);
+            bw.newLine();
+            bw.write("" + answers.size());
             bw.newLine();
             for (String answer : answers) {
                 bw.write(answer);
@@ -81,4 +79,35 @@ public class UserThread extends Thread {
             e.printStackTrace();
         }
     }
+
+    private void flushPollData(String pollID) throws PollNotFoundException {
+        File users = new File("Polls/" + pollID + ".txt");
+        if (!users.exists()) {
+            throw new PollNotFoundException("Poll with that ID does not exist!");
+        }
+        try (BufferedReader br = new BufferedReader(new FileReader(users))) {
+            String question = br.readLine();
+            writer.write(question);
+            writer.println();
+            String numOfAnswers = br.readLine();
+            try {
+                int num = Integer.parseInt(numOfAnswers);
+                System.out.println(num);
+                writer.write(numOfAnswers);
+                writer.println();
+                for (int i = 0; i < num; i++) {
+                    String answer = br.readLine();
+                    writer.write(answer);
+                    writer.println();
+                }
+                writer.flush();
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+            System.out.println(numOfAnswers);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
